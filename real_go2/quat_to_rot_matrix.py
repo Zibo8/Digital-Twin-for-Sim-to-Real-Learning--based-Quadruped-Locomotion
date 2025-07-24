cce440d14530ecbba59ee03da729e1c0c7be9eb3
Zibo <zibo@example.com>
2025-07-24 17:03:18 +0200

    files in ubuntu

---
+++ real_go2/quat_to_rot_matrix.py
@@ -0,0 +1,31 @@
+import numpy as np
+import math
+def quat_to_rot_matrix(quat):
+    """
+    Get rotation matrix from the given quaternion.
+    Args:
+        quat (np.array[float[4]]): quaternion [w,x,y,z]
+    Returns:
+        np.array[float[3,3]]: rotation matrix.
+    """
+    w, x, y, z = quat
+    r = np.arctan2(2 * (w * x + y * z), 1 - 2 * (x ** 2 + y ** 2))
+    p = np.arcsin(2 * (w * y - z * x))
+    y = np.arctan2(2 * (w * z + x * y), 1 - 2 * (y ** 2 + z ** 2))
+    R_x = np.array([[1, 0, 0],
+                    [0, math.cos(r), -math.sin(r)],
+                    [0, math.sin(r), math.cos(r)]
+                    ])
+
+    R_y = np.array([[math.cos(p), 0, math.sin(p)],
+                    [0, 1, 0],
+                    [-math.sin(p), 0, math.cos(p)]
+                    ])
+
+    R_z = np.array([[math.cos(y), -math.sin(y), 0],
+                    [math.sin(y), math.cos(y), 0],
+                    [0, 0, 1]
+                    ])
+
+    rot = np.dot(R_z, np.dot(R_y, R_x))  
+    return rot  
